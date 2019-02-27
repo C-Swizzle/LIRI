870703d74cb3236187fdfb66cf2fb661f57f1eef
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-02-26 18:30:40 -0800

    .

--- liri.js
+++ liri.js
@@ -5,8 +5,10 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify=require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-if(commandToDo==="concert-this"){
+var axios=require("axios");
 
+if(commandToDo==="concert-this"){
+    bandInTownLookup(otherInput);
 }else if(commandToDo==="spotify-this-song"){
     spotifyLookup(otherInput);
 } else if(commandToDo==="movie-this"){
@@ -21,21 +23,37 @@ function spotifyLookup(str){
     .search({ type: 'track', query: str, limit:5})
     .then(function(response) {
         console.log(JSON.stringify(response,null,2));
-      var primaryArist = response.tracks.items[0].album.artists[0].name;
-      var otherArist = response.tracks.items[1].album.artists[0].name;
-      var otherArist = response.tracks.items[1].album.artists[0].name;
+    //   var primaryArist = response.tracks.items[0].album.artists[0].name;
+    //   var otherArist = response.tracks.items[1].album.artists[0].name;
+    //   var otherArist = response.tracks.items[1].album.artists[0].name;
       for (var i=0;i<response.tracks.items.length;i++){
           console.log("Artist: "+response.tracks.items[i].album.artists[0].name +"\n");
           console.log("Album: "+response.tracks.items[i].album.name +"\n");
-
+          console.log("song: "+response.tracks.items[i].name +"\n");
+          console.log("preview: "+response.tracks.items[i].preview_url +"\n");
+          console.log("------------------------------------------------------------------");
       }
 
 
-      console.log("Primary artist: "+primaryArist);
-      console.log("second artist: "+otherArist);
+    //   console.log("Primary artist: "+primaryArist);
+    //   console.log("second artist: "+otherArist);
 
     })
     .catch(function(err) {
       console.log(err);
     });
 }
+
+function bandInTownLookup(str){
+    axios.get("https://rest.bandsintown.com/artists/" + str +  "?app_id=anything")
+    .then(function(response){
+        console.log(response);
+    })
+};
+
+// axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+//   function(response) {
+//     // Then we print out the imdbRating
+//     console.log("The movie's rating is: " + response.data.imdbRating);
+//   }
+// );
